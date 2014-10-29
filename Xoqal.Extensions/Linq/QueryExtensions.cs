467c4cf061057182f107ca616980585f180b7f3a
Soren Z.Vand <sunrise.freedom@gmail.com>
2014-10-29 10:30:50 +0330

    - Add Exnesions Project. - Add QueryExtensions. - Add UrlHelper

---
+++ Xoqal.Extensions/Linq/QueryExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Xoqal.Extensions.Linq
+{
+    /// <summary>
+    /// IQueryable Helpers
+    /// </summary>
+    public static class QueryExtensions
+    {
+        /// <summary>
+        /// 'Select' by condition before query executed in provider.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="condition">the condition</param>
+        /// <param name="trueExpression">The expression that will apply if <see cref="condition"/> is true.</param>
+        /// <param name="falseExpression">The expression that will apply if <see cref="condition"/> is false.</param>
+        /// <returns>
+        /// filtered query
+        /// </returns>
+        public static IQueryable<TResult> SelectIf<TSource, TResult>(
+            this IQueryable<TSource> source,
+            bool condition,
+            Expression<Func<TSource, TResult>> trueExpression,
+            Expression<Func<TSource, TResult>> falseExpression)
+        {
+            return condition ? source.Select(trueExpression) : source.Select(falseExpression);
+        }
+
+        /// <summary>
+        /// Conditional 'Where', decide about predicate before query executed in provider.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="condition">the condition</param>
+        /// <param name="trueExpression">The expression that will apply if <see cref="condition"/> is true.</param>
+        /// <param name="falseExpression">The expression that will apply if <see cref="condition"/> is false.</param>
+        /// <returns>
+        /// filtered query
+        /// </returns>
+        public static IQueryable<T> WhereIf<T>(
+            this IQueryable<T> source,
+            bool condition,
+            Expression<Func<T, bool>> trueExpression,
+            Expression<Func<T, bool>> falseExpression)
+        {
+            return condition ? source.Where(trueExpression) : source.Where(falseExpression);
+        }
+
+        /// <summary>
+        /// LINQ Between Operator
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        /// <example>var query = db.People.Between(person => person.Age, 18, 21);</example>
+        /// <remarks>http://stackoverflow.com/questions/1447635/linq-between-operator</remarks>
+        public static IQueryable<TSource> Between<TSource, TKey>
+            (this IQueryable<TSource> source,
+                Expression<Func<TSource, TKey>> keySelector,
+                TKey low, TKey high) where TKey : IComparable<TKey>
+        {
+            if (keySelector == null)
+                throw new ArgumentNullException("keySelector");
+
+            Expression key = Expression.Invoke(keySelector,
+                keySelector.Parameters.ToArray());
+
+            Expression lowerBound = Expression.GreaterThanOrEqual
+                (key, Expression.Constant(low));
+
+            Expression upperBound = Expression.LessThanOrEqual
+                (key, Expression.Constant(high));
+
+            Expression and = Expression.AndAlso(lowerBound, upperBound);
+
+            Expression<Func<TSource, bool>> lambda =
+                Expression.Lambda<Func<TSource, bool>>(and, keySelector.Parameters);
+
+            return source.Where(lambda);
+        }
+    }
+}
---
+++ Xoqal.Extensions/MVC/UrlHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Xoqal.Extensions.MVC
+{
+    /// <summary>
+    /// Provide some helpers method toward of URL.
+    /// </summary>
+    public static class UrlHelper
+    {
+        private static readonly Regex Feet = new Regex(@"([0-9]\s?)'([^'])", RegexOptions.Compiled);
+        private static readonly Regex Inch1 = new Regex(@"([0-9]\s?)''", RegexOptions.Compiled);
+        private static readonly Regex Inch2 = new Regex(@"([0-9]\s?)""", RegexOptions.Compiled);
+        private static readonly Regex Num = new Regex(@"#([0-9]+)", RegexOptions.Compiled);
+        private static readonly Regex Dollar = new Regex(@"[$]([0-9]+)", RegexOptions.Compiled);
+        private static readonly Regex Percent = new Regex(@"([0-9]+)%", RegexOptions.Compiled);
+        private static readonly Regex Sep = new Regex(@"[\s_/\\+:.]", RegexOptions.Compiled);
+
+        // included Persian char(s)
+        private static readonly Regex Empty = new Regex(@"[^-A-Za-z0-9\u0600-\u06FF]", RegexOptions.Compiled);
+        private static readonly Regex Extra = new Regex(@"[-]+", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Gets readable encoded URL from the specified raw URL.
+        /// </summary>
+        /// <param name="rawUrl"></param>
+        /// <returns></returns>
+        /// <remarks>Persian characters included.</remarks>
+        public static string GetReadableEncodedUrl(string rawUrl)
+        {
+            if (!string.IsNullOrEmpty(rawUrl))
+            {
+                rawUrl = rawUrl.Trim().ToLower();
+                rawUrl = rawUrl.Replace("&", "and");
+
+                rawUrl = Feet.Replace(rawUrl, "$1-ft-");
+                rawUrl = Inch1.Replace(rawUrl, "$1-in-");
+                rawUrl = Inch2.Replace(rawUrl, "$1-in-");
+                rawUrl = Num.Replace(rawUrl, "num-$1");
+
+                rawUrl = Dollar.Replace(rawUrl, "$1-dollar-");
+                rawUrl = Percent.Replace(rawUrl, "$1-percent-");
+
+                rawUrl = Sep.Replace(rawUrl, "-");
+
+                rawUrl = Empty.Replace(rawUrl, string.Empty);
+                rawUrl = Extra.Replace(rawUrl, "-");
+
+                rawUrl = rawUrl.Trim('-');
+            }
+
+            return rawUrl;
+        }
+    }
+}
