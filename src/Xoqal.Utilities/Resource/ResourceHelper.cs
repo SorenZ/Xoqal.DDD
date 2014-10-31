acbc41fc98a92b18af73c24a3d88ad08cf885f33
Soren Z.Vand <sunrise.freedom@gmail.com>
2014-10-31 14:04:49 +0330

    - Add Resouce Helper - Add Text Extensions

---
+++ src/Xoqal.Utilities/Resource/ResourceHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace Xoqal.Utilities.Resource
+{
+    /// <summary>
+    /// Helps to resolve the resource keys.
+    /// </summary>
+    public class ResourceHelper
+    {
+        /// <summary>
+        /// Gets the resource value.
+        /// </summary>
+        /// <param name="resourceTypeName"> Name of the resource type. </param>
+        /// <param name="name"> The name. </param>
+        /// <returns> </returns>
+        public static string GetResourceValue(string resourceTypeName, string name)
+        {
+            return GetResourceValue(Type.GetType(resourceTypeName), name);
+        }
+
+        /// <summary>
+        /// Gets the resource value.
+        /// </summary>
+        /// <param name="resourceType"> Type of the resource. </param>
+        /// <param name="name"> The name. </param>
+        /// <returns> </returns>
+        public static string GetResourceValue(Type resourceType, string name)
+        {
+            var property = resourceType.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+            return property != null ? property.GetValue(null, null).ToString() : string.Empty;
+        }
+    }
+}
---
+++ src/Xoqal.Utilities/Text/TextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Xoqal.Utilities.Text
+{
+    public static class TextExtensions
+    {
+        /// <summary>
+        /// Gets the summary.
+        /// </summary>
+        /// <param name="text"> The text. </param>
+        /// <param name="wordCount"> The word count. </param>
+        /// <returns> </returns>
+        public static string GetSummary(this string text, int wordCount)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty;
+
+            var parts = text.Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return string.Join(" ", parts.Take(wordCount).ToArray());
+        }
+    }
+}
